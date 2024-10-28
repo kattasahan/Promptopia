bc07ca1b52549f0759305cd3402d5c1748598c36
Katta, Sahan <Sahan.Katta@carelon.com>
2024-10-28 15:22:30 +0530

    =

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
 
 function EditPrompt() {
   const router = useRouter();
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
 
