6cf78f41a23a50c3a0f25e022c5f1a9268c0f0fd
Katta, Sahan <Sahan.Katta@carelon.com>
2024-10-28 15:29:57 +0530

    -

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 // import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 import Form from "@components/Form";
 
@@ -52,13 +53,15 @@ function EditPrompt() {
   };
 
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense>
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
   );
 }
 
